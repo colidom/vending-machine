58ce422b2d4a555f434756fbda9f7ecadb32364d
colidom <colidom@outlook.com>
2023-02-24 17:11:30 +0000

    Formateo

--- vending.py
+++ vending.py
@@ -8,7 +8,6 @@ def init_info(input_path: Path) -> list:
 
 
 def operations(input_path: Path) -> list:
-
     with open(input_path, "r") as f:
         data = [line.strip() for line in f]
         return data
