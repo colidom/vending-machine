88bad94a58e72e2b157dfc4b5e3ed29e6b269542
colidom <colidom@outlook.com>
2023-02-24 17:10:34 +0000

    Añadida función de lectura de fichero de operaciones

--- vending.py
+++ vending.py
@@ -8,5 +8,13 @@ def init_info(input_path: Path) -> list:
         return data
 
 
+def operations(input_path: Path) -> list:
+
+    with open(input_path, "r") as f:
+        data = [line.strip() for line in f]
+        return data
+
+
 if __name__ == "__main__":
     init_info("input_data/vending.dat")
+    operations("input_data/operations.dat")
