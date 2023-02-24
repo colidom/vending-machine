4787f256296c86693b4976176793f72bf9cc780e
colidom <colidom@outlook.com>
2023-02-24 17:14:43 +0000

    Añadida función de escritura de fichero de salirda

--- vending.py
+++ vending.py
@@ -13,6 +13,13 @@ def operations(input_path: Path) -> list:
         return data
 
 
+def status_machine(output_path: Path) -> str:
+    with open(output_path, "w") as f:
+        data = "Hello World"
+        f.write(f"{data}")
+
+
 if __name__ == "__main__":
     init_info("input_data/vending.dat")
     operations("input_data/operations.dat")
+    status_machine("output_data/status.dat")
