05b79e2ae94d7fd78b5e7d373bc07b38ed297035
colidom <colidom@outlook.com>
2023-02-28 18:45:18 +0000

    Portado el código actual a la propuesta del profesor

--- vending-org.py
+++ vending-org.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
 
-def init_info(input_path: Path) -> list:
-    with open(input_path, "r") as f:
-        data = [line.strip() for line in f]
-        return data
+# def init_info(input_path: Path) -> list:
+#     with open(input_path, "r") as f:
+#         data = [line.strip() for line in f]
+#         return data
 
 
 def operations(input_path: Path) -> list:
     with open(input_path, "r") as f:
         data = [line.strip() for line in f]
-        return data
+        print(data)
 
 
 def status_machine(output_path: Path) -> str:
@@ -20,6 +20,5 @@ def status_machine(output_path: Path) -> str:
 
 
 if __name__ == "__main__":
-    init_info("input_data/vending.dat")
-    operations("input_data/operations.dat")
-    status_machine("output_data/status.dat")
+    operations("data/vending/operations.dat")
+    status_machine("data/vending/status.dat")
--- vending.py
+++ vending.py
@@ -5,12 +5,25 @@ import filecmp
 from pathlib import Path
 
 
+def operations(input_path: Path) -> list:
+    with open(input_path, "r") as f:
+        data = [line.strip() for line in f]
+        return data
+
+
+def status_machine(output_path: Path) -> str:
+    with open(output_path, "w") as f:
+        data = "Hello World"
+        f.write(f"{data}")
+
+
 def run(operations_path: Path) -> bool:
-    status_path = 'data/vending/status.dat'
+    status_path = "data/vending/status.dat"
     # TU CÓDIGO AQUÍ
 
-    return filecmp.cmp(status_path, 'data/vending/.expected', shallow=False)
+    return filecmp.cmp(status_path, "data/vending/.expected", shallow=False)
 
 
-if __name__ == '__main__':
-    run('data/vending/operations.dat')
+if __name__ == "__main__":
+    operations("data/vending/operations.dat")
+    status_machine("data/vending/status.dat")
